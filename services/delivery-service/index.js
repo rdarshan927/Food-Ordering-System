51306c3a6106a5e0f21777cbd56208812dcf9ad3
rdarshan927 <rdarshan927@gmail.com>
2025-03-29 15:45:33 +0530

    initialized mern micro service

---
+++ services/delivery-service/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db.js");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => res.send("API is running..."));
+
+const PORT = process.env.PORT || 5001;
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = server;
