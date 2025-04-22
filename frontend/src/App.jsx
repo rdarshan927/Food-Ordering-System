6647fc9a57cdc9ce533be7cbeeca6553afd3b086
susara-perera <susaraperera33.com>
2025-04-22 11:37:16 +0530

    add payment method form
    add payment method form and update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,31 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import Checkout from "./pages/payment/Checkout.jsx"; 
-import PayPalButton from "./components/PayPalButton.jsx"; 
+//import PayPalButton from "./components/PayPalButton.jsx"; 
+import StripeCheckout from "./components/StripeCheckout.jsx";
+import AddPaymentMethod from "./components/AddPaymentMethod.jsx";
 
+// function App() {
+//   return (
+//     <PayPalScriptProvider options={{ "client-id": "YOUR_PAYPAL_CLIENT_ID" }}>
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<h1>Home Page</h1>} />
+//           <Route path="/checkout" element={<Checkout />} />
+//         </Routes>
+//       </Router>
+//     </PayPalScriptProvider>
+//   );
+// }
 function App() {
   return (
-    <PayPalScriptProvider options={{ "client-id": "YOUR_PAYPAL_CLIENT_ID" }}>
       <Router>
-        <Routes>
-          <Route path="/" element={<h1>Home Page</h1>} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
+          <Routes>
+              <Route path="/" element={<h1>Home Page</h1>} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="add-payment" element={<AddPaymentMethod/>}/>
+          </Routes>
       </Router>
-    </PayPalScriptProvider>
   );
 }
 
